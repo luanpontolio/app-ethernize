c2e0ed23f9f2a9b6c791dee383eb16368d0b3966
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-09-10 15:01:05 -0300

    feat: Include ThemePRovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { StylesProvider } from "@material-ui/styles";
+import { createTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from 'styled-components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const defaultTheme = createTheme();
+
+  console.log("Default theme passing to ThemeProvider", defaultTheme);
+
+  return (
+    <StylesProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StylesProvider>
+  )
 }
 
 export default MyApp
