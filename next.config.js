ccf9f2a096944914e33eb2a1195835c169ded595
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-09-10 13:01:35 -0300

    feat: add web3 and web3modal

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL
+  },
+  images: {
+    deviceSizes: [320, 768, 1024, 1400],
+    domains: ['*'],
+    imageSizes: [16, 32, 48, 56, 64, 96, 128, 256, 384]
+  }
 }
 
 module.exports = nextConfig
